ddd83a2427903a7ca0680bf8323e861d5a3adffd
Cory Martin <cory.r.martin@noaa.gov>
2026-08-13 15:58:17 +0000

    Commit before lunch

--- src/geode/ingest/watcher.py
+++ src/geode/ingest/watcher.py
@@ -95,6 +95,8 @@ def on_message(client, userdata, msg):
     if not url:
         url = data.get("url")
         filename = data.get("filename")
+        print(f"url: {url}")
+        print(f"filename: {filename}")
 
     if url and filename:
         download_file(url, filename)
