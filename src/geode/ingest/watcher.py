690e50f585f114b3c6a6af3d98a8efa8d3f0d0c0
Cory Martin <cory.r.martin@noaa.gov>
2026-08-13 17:22:51 +0000

    more debug prints

--- src/geode/ingest/watcher.py
+++ src/geode/ingest/watcher.py
@@ -11,16 +11,13 @@ import requests
 BROKER_ADDRESS = "wis2node.globaldata.nws.noaa.gov"
 BROKER_PORT = 443
 TOPIC = "origin/a/wis2/us-noaa-nws/data/core/weather/#"
-DOWNLOAD_DIR = "./wis2-data-tmp"
-
-# Ensure the download directory exists
-os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+DOWNLOAD_DIR_ROOT = "./wis2-data-tmp"
 
 
 # ==========================================
 # Core Functions
 # ==========================================
-def download_file(url, filename):
+def download_file(url, download_dir, filename):
     """Downloads a file over HTTP(S) and saves it locally."""
     try:
         print(f"[*] Starting download: {url}")
@@ -31,7 +28,10 @@ def download_file(url, filename):
 
         # Sanitize filename
         safe_filename = os.path.basename(filename)
-        filepath = os.path.join(DOWNLOAD_DIR, safe_filename)
+        filepath = os.path.join(DOWNLOAD_DIR_ROOT, download_dir, filename)
+
+        # ensure the directory exists
+        os.makedirs(os.path.dirname(filepath), exist_ok=True)
 
         temporary_filepath = f"{filepath}.part"
         with open(temporary_filepath, "wb") as f:
@@ -99,7 +99,8 @@ def on_message(client, userdata, msg):
     if url and filename:
         print(f"url: {url}")
         print(f"filename: {filename}")
-        download_file(url, filename)
+        print(msg.topic.split["/"])
+        download_file(url, msg.topic, filename)
     else:
         print("[-] Could not find a valid download URL in the payload.")
         print(
