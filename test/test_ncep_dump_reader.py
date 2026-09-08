d0991a2c0dfee62f2463fb81ba2697c86f164ffc
Ron McLaren <ronald.mclaren@noaa.gov>
2026-09-08 15:57:38 +0000

    norm

--- test/test_ncep_dump_reader.py
+++ test/test_ncep_dump_reader.py
@@ -11,6 +11,7 @@ from geode.configs import ncep_dump_config
 from geode.configs.geode_config import geode_config
 from geode.ingest.consumers import ncep_dump_reader
 
+
 DataDir = os.path.join(os.path.dirname(__file__), "data")
 DownloadDir = DataDir
 DumpDir = os.path.join(DataDir, "dump")
